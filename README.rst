8eeab7006f2bcd13803147521e0fa3003b33aeb6
Steven Douglas <steven@douglas-enterprises.com>
2017-09-05 23:43:29 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,3 @@
+bweb - bhyve web interface
+
+This interface is passed a comma delimited string of bapi api enabled hosts via the environment. It feeds these to the bapiclient.
---
+++ bweb/web.py
@@ -0,0 +1,29 @@
+import os
+from bapiclient import client
+from flask import Flask, jsonify, render_template
+
+# Where are our jinja templates?
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+@app.route('/')
+def vm_list():
+    vms = client.get_all_vms()
+    vms_detail = []
+    for vm in vms:
+        vms_detail.append(client.get_vm_details(vm))
+    return render_template('vm_manager.html', vms=vms_detail)
+
+@app.route('/vm/<vm_name>/<action>')
+def vm_action(vm_name, action):
+    output = client.vm_action(vm_name, action)
+    print output
+    try:
+        output['error']
+        return jsonify(output), 500
+    except KeyError:
+        return jsonify(output), 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=5001)
